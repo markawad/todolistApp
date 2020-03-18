0343d203561def120a2b9f9b6ac423e2ec3f33db
markawad <mark.awad8@gmail.com>
2020-03-18 23:47:01 +0100

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -10,9 +12,32 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # Mark has heard of a new lists app and goes to the website to check it out
         self.browser.get("http://localhost:8000")
+
+        # He notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail("finish the test!")
+        headerText = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', headerText)
+
+        # He is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), "Enter a to-do item")
+
+        # He types Buy feathers into the text box
+        inputbox.send_keys('Buy feathers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy feathers') for row in rows) 
+
+        # There is still a textbox initing him to add another item
+        
+        # Mark enters "Use feathers for mask"
+        self.fail("Finish the test")
+
 
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
