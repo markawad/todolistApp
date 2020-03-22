56d79800c73422cd94b0a289eb00235cbd179c5f
Mark Awad <mark.awad8@gmail.com>
2020-03-22 14:58:06 +0100

    Site just handles 1 item, will refactor

--- functional_tests.py
+++ functional_tests.py
@@ -27,11 +27,20 @@ class NewVisitorTest(unittest.TestCase):
         # He types Buy feathers into the text box
         inputbox.send_keys('Buy feathers')
         inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(1)
+
+        # He types another input
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Clean feathers')
+        inputbox.send_keys(Keys.ENTER)
+
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy feathers' for row in rows), "New to-do item did not appear in the table") 
+        self.assertIn('1: Buy feathers', [row.text for row in rows])
+        self.assertIn('2: Clean feathers', [row.text for row in rows])
 
         # There is still a textbox initing him to add another item
         
