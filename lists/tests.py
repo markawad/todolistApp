7f98c49f274df396d3a5a28337bdede262d21636
Mark Awad <mark.awad8@gmail.com>
2020-03-23 03:11:17 +0100

    Created list model

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
@@ -12,16 +12,24 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retreiving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = "The first (ever) list item"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "Second item"
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -29,7 +37,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Second item')
+        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
     
@@ -38,8 +48,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text="lorem")
-        Item.objects.create(text="ipsum")
+        list_ = List.objects.create()
+        Item.objects.create(text="lorem", list=list_)
+        Item.objects.create(text="ipsum", list=list_)
         
         response = self.client.get('/lists/first_list/')
 
