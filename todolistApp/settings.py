320406fe3614972cf95a025d559e1a37e496e4f1
Mark Awad <mark.awad8@gmail.com>
2020-03-28 01:25:55 +0100

    Hack allowed_hosts to be *

--- todolistApp/settings.py
+++ todolistApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e8fgp9-%x9$=1-*kxuv-n7bhlx_mh1_16+7weelu^95=g&l_ak'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
