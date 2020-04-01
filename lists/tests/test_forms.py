94f2475dfc8f5b3a00dfacc0e9635bd0f4bbeb7b
Mark Awad <mark.awad8@gmail.com>
2020-04-02 01:47:20 +0200

    add form.save functionality and simplify views.py

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.forms import ItemForm
+from lists.models import Item, List
 
 class ItemFormTest(TestCase):
     
@@ -16,3 +17,8 @@ class ItemFormTest(TestCase):
             ["You can't have an empty list item."]
         )
     
+    def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
