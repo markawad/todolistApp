2f8cce50cd030ad3bd997622cb1330a59216f0cb
Mark Awad <mark.awad8@gmail.com>
2020-04-03 19:56:37 +0200

    add jquery, qunit tests, list.js with keypress listeners

---
+++ lists/static/list.js
@@ -0,0 +1,9 @@
+var initialize = function () {
+    $('input[name="text"]').on('keypress', function() {
+        $('.has-error').hide();
+    });
+}
+
+if ($('.has-error').is(':visible')){
+    $('input[name="text"]').val('');
+}
